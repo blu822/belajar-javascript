d7bad52a731cd6b80e7e3d878953e1a9079b8e82
Adit <blueberrybm22@gmail.com>
2024-01-29 13:35:33 +0700

    variabel array

---
+++ array.js
@@ -0,0 +1,8 @@
+// var nama array = new Array()
+var lemari = new Array("baju", 1200000, true);
+console.log(lemari[2]);
+console.log(lemari[1]);
+
+lemari[3] = "ktp";
+lemari.push("ATM");
+console.log(lemari);
